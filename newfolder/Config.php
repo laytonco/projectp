da7302e94606c3046c241c1a1306940d4730de2c
laytonco <laytonco@users.noreply.github.com>
2017-06-11 14:30:16 -0500

    Create Config.php

--- newfolder/Config.php
+++ newfolder/Config.php
@@ -1,7 +1,7 @@
 <?php
 Define('DB_server', 'localhost:3036');
 Define('DB_USERNAME', 'root');
-Define('DB_PASSWORD', 'rootpassword');
+Define('DB_PASSWORD', 'Skullcandy4!');
 Define('DB_DATABASE', 'database');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
